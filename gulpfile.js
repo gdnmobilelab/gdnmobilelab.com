c8c4609171bcd9173276655cd03251f61b8dd374
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-05-10 17:25:57 -0400

    use gulp-connect instead

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,37 @@ var gulp = require('gulp'),
 
 gulp.task('build-html', function() {
     gulp.src('src/**/*.html')
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist'))
+        .pipe(plugins.connect.reload());
 });
+
+gulp.task('build-assets', function() {
+    gulp.src(['src/assets/**/*'], {base: 'src'})
+        .pipe(gulp.dest('./dist'))
+        .pipe(plugins.connect.reload());
+})
 
 gulp.task('build-css', function() {
     gulp.src('src/sass/style.scss')
         .pipe(plugins.sass.sync().on('error', plugins.sass.logError))
         .pipe(gulp.dest('./dist'))
+        .pipe(plugins.connect.reload());
 });
 
 gulp.task('serve', function() {
-    gulp.src('dist')
-        .pipe(plugins.webserver({
-            livereload: true,
-            open: "http://localhost:8000/index.html"
-        }));
+    plugins.connect.server({
+        root: 'dist',
+        livereload: true
+    });
+    gulp.src('./').
+    pipe(plugins.open({uri: 'http://localhost:8080'}));
 });
 
 gulp.task('watch', function() {
     gulp.watch('src/**/*.html', ['build-html']);
+    gulp.watch('src/assets/**/*', ['build-assets']);
     gulp.watch('src/sass/**/*.scss', ['build-css']);
 });
 
-gulp.task('build', ['build-html', 'build-css']);
+gulp.task('build', ['build-html', 'build-assets', 'build-css']);
 gulp.task('dev', ['build', 'serve', 'watch']);

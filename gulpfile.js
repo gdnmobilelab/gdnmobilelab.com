cb08d32f032e57b27e0770243a8da0b21f5b037a
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-05-10 16:16:55 -0400

    watch html for changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')();
 
-gulp.task('default', function() {
-	console.log('gulpy')
+gulp.task('copy-html', function() {
+    gulp.src('src/**/*.html')
+        .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('serve', function() {
+    gulp.src('dist')
+        .pipe(plugins.webserver({
+            livereload: true,
+            open: "http://localhost:8000/index.html"
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.html', ['copy-html']);
+})
+
+gulp.task('build', ['copy-html']);
+gulp.task('dev', ['build', 'serve', 'watch']);
